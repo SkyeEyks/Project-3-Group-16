2b703cffd2febfea6a232c2889a720b6a7b096d4
Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>
2021-03-31 17:16:10 +0200

    .

---
+++ src/main/java/za/ac/cput/Calc.java
@@ -0,0 +1,30 @@
+package za.ac.cput;
+
+public class Calc {
+    public Calc() {
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public long multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        double result;
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisor cannot divide by zero");
+        } else {
+            result = Double.valueOf(a) / Double.valueOf(b);
+        }
+        return result;
+    }
+
+
+}
